b7b7511d269830a9a3778ef128cd769034bf4819
Bassil Virk <bassillooney@outlook.com>
2023-04-20 18:00:51 -0400

    Delete old cleanup files

--- DropFired.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("Intermediates/deltaTransformed.csv")
-
-indicesToRemove = df[df["Fired"] == True].index
-df.drop(indicesToRemove, inplace=True)
-df.drop(columns=["Fired", "HitTarget"], inplace=True)
-df.to_csv("Intermediates/droppedFires.csv", index=False)
